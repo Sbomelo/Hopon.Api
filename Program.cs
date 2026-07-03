e1212ca1e7477d44cfc5ada79056d11765cce32a
Sbomelo <s225118440@mandela.ac.za>
2026-07-03 10:02:47 +0200

    Registered the DbContext in program.cs

--- Program.cs
+++ Program.cs
@@ -1,9 +1,15 @@
+using Hopon.Api.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<HoponDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("HoponDb")));
 
 var app = builder.Build();
 
