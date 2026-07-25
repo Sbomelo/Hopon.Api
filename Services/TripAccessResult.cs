db03eb6c279c4b2522984223ca4ab33e7d19a9b3
vimba <vimbamzaza@gmail.com>
2026-07-25 18:45:36 +0200

    Trip access result service

--- Services/TripAccessResult.cs
+++ Services/TripAccessResult.cs
@@ -1,13 +1,20 @@
 ﻿using Hopon.Api.Models;
 
-namespace Hopon.Api.Services
+namespace Hopon.Api.Services;
+
+public class TripAccessResult
 {
-    public class TripAccessResult
-    {
+<<<<<<< HEAD
         public bool HasAccess { get; set; }
-        public bool  IsTrackingActive { get; set; }
+
+        public bool IsTrackingActive { get; set; }
+
         public Ticket? Ticket { get; set; }
         public Trip? Trip { get; set; }
+=======
+    public class TripAccessResult
+    {
 
+>>>>>>> parent of cf11f90 (Tripp Access filter)
     }
-}
+
