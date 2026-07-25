11db6fca44e16b8a455bec7dbb0a4aad898abd76
vimba <vimbamzaza@gmail.com>
2026-07-25 17:51:05 +0200

    Migrations for the spelling fix on the Tickets table

---
+++ Migrations/20260725154548_SeaatNumberSpellingFixInTicketsTable.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hopon.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeaatNumberSpellingFixInTicketsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "SetNumber",
+                table: "Tickets",
+                newName: "SeatNumber");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "SeatNumber",
+                table: "Tickets",
+                newName: "SetNumber");
+        }
+    }
+}
