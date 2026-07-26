a59d19a50aacfd32acf4cca41fd5f6ec94bbbf50
vimba <vimbamzaza@gmail.com>
2026-07-27 01:27:35 +0200

    Tripdashboard service

---
+++ Services/ITripDashboardService.cs
@@ -0,0 +1,9 @@
+﻿using Hopon.Api.DTOs.Trips;
+
+namespace Hopon.Api.Services
+{
+    public interface ITripDashboardService
+    {
+        Task<MyTripsResponseDto> GetMyTripsAsync(int userId);
+    }
+}
---
+++ Services/TripDashboard.cs
@@ -0,0 +1,67 @@
+﻿using Hopon.Api.Data;
+using Hopon.Api.DTOs.Trips;
+using Hopon.Api.Models.Enums;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hopon.Api.Services
+{
+    public class TripDashboardService : ITripDashboardService
+    {
+        private readonly HoponDbContext _context;
+
+        public TripDashboardService(HoponDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public async Task<MyTripsResponseDto> GetMyTripsAsync(int userId)
+        {
+            var tickets = await _context.Tickets
+                                        .Where(t => t.UserId == userId && t.IsActive)
+                                        .Include(t => t.Trip)
+                                            .ThenInclude(trip => trip.BusRoute)
+                                        .AsNoTracking()
+                                        .ToListAsync();
+
+            var response = new MyTripsResponseDto();
+
+            foreach (var ticket in tickets)
+            {
+                var trip = ticket.Trip;
+
+                var dto = new MyTripDto
+                {
+                    TripId = trip.Id,
+                    TicketReference = ticket.TicketReference,
+                    RouteName = trip.BusRoute.Name,
+                    TripDate = trip.ScheduledDeparture.Date,
+                    ScheduledDeparture = trip.ScheduledDeparture,
+                    ScheduledArrival = trip.ScheduledArrival,
+                    ActualDepature = trip.ActualDeparture,
+                    ActualArrival = trip.ActualArrival,
+                    Status = trip.Status.ToString(),
+                    IsLive = TripStatusRules.IsTrackingActive(trip.Status)
+                };
+
+                if (dto.IsLive)
+                    response.LiveTrips.Add(dto);
+
+                else
+                    response.PastTrips.Add(dto);
+
+            }
+
+            response.LiveTrips = response.LiveTrips
+                                         .OrderBy(t => t.ScheduledArrival)
+                                         .ToList();
+
+            response.PastTrips = response.PastTrips
+                                         .OrderByDescending(t => t.ScheduledArrival)
+                                         .ToList();
+
+            return response;
+        }
+    }
+    
+}
