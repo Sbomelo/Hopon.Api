03424a195821f0b13f2f222d3f3f0c63e99f91fa
vimba <vimbamzaza@gmail.com>
2026-07-27 01:29:52 +0200

    enum for trip status rules

---
+++ Models/Enums/TripStatusRules.cs
@@ -0,0 +1,11 @@
+﻿namespace Hopon.Api.Models.Enums
+{
+    public static class TripStatusRules
+    {
+        public static bool IsTrackingActive(TripStatus status)
+        {
+            return status is TripStatus.Scheduled or TripStatus.InProgress or TripStatus.Delayed;
+        }
+
+    }
+}
