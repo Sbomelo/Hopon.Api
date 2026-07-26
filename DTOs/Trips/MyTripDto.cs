c7e2dada8341e00d743ff64b7e7365480e300c2c
vimba <vimbamzaza@gmail.com>
2026-07-27 01:26:59 +0200

    DTOs for requests of active trips and trip history, MyTripDto and MyTripResponseDto

---
+++ DTOs/Trips/MyTripDto.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Principal;
+
+namespace Hopon.Api.DTOs.Trips
+{
+    public class MyTripDto
+    {
+        public int TripId { get; set; }
+        public string TicketReference { get; set; } = null!;
+        public string RouteName { get; set; } = null!;
+        public DateTime TripDate{ get; set; }
+        public DateTime ScheduledDeparture { get; set; }
+        public DateTime ScheduledArrival { get; set; }
+        public DateTime? ActualDepature { get; set; }
+        public DateTime? ActualArrival { get; set; }
+        public string Status { get; set; } = null!;
+        public bool IsLive { get; set; }
+    }
+}
---
+++ DTOs/Trips/MyTripsResponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Hopon.Api.DTOs.Trips
+{
+    public class MyTripsResponseDto
+    {
+        public List<MyTripDto> LiveTrips { get; set; } = new();
+        public List<MyTripDto> PastTrips { get; set; } = new();
+    }
+}
