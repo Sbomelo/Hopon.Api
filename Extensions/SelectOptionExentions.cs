bde7fc479b384433dbd65d081cd41cdcb9cece94
vimba <vimbamzaza@gmail.com>
2026-07-28 23:30:58 +0200

    Added Extension helpers

---
+++ Extensions/SelectOptionExentions.cs
@@ -0,0 +1,12 @@
+﻿using Hopon.Api.DTOs.Admin;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Hopon.Api.Extensions;
+
+public static class SelectOptionExtensions
+{
+    public static List<SelectListItem> ToSelectListItems(this IEnumerable<SelectOptionDto> options)
+    {
+        return options.Select(o => new SelectListItem(o.Label, o.Id.ToString())).ToList();
+    }
+}
